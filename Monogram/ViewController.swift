d61b5a4a3583226af9bc33fec8b955abe19572d6
Rasim DEMIRBAY <rasim.demirbay@gltmac120-mac.local>
2017-02-12 14:25:22 +0300

    Initial Commit

---
+++ Monogram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Monogram
+//
+//  Created by Rasim DEMIRBAY on 12/02/2017.
+//  Copyright © 2017 Rasim DEMIRBAY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MonogramTests/MonogramTests.swift
@@ -0,0 +1,36 @@
+//
+//  MonogramTests.swift
+//  MonogramTests
+//
+//  Created by Rasim DEMIRBAY on 12/02/2017.
+//  Copyright © 2017 Rasim DEMIRBAY. All rights reserved.
+//
+
+import XCTest
+@testable import Monogram
+
+class MonogramTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
